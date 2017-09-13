ec222698a183c53516b12a198279fa378cba544c
rhotep <andreas.pittrich@posteo.de>
2017-09-13 17:45:55 +0200

    test

---
+++ test.js
@@ -0,0 +1,52 @@
+var createApp = require('github-app');
+
+
+
+var app = createApp({
+  id: process.env.APP_ID,
+  cert: require('fs').readFileSync('private-key.pem')
+});
+
+
+
+
+
+var createHandler = require('github-webhook-handler');
+
+var handler = createHandler({
+  path: '/',
+  secret: 'myhashsecret'
+})
+
+handler.on('issues', function (event) {
+  if (event.payload.action === 'opened') {
+    var installation = event.payload.installation.id;
+
+    app.asInstallation(installation).then(function (github) {
+      github.issues.createComment({
+        owner: event.payload.repository.owner.login,
+        repo: event.payload.repository.name,
+        number: event.payload.issue.number,
+        body: 'Welcome to the robot uprising.'
+      });
+    });
+  }
+});
+
+
+
+
+
+var http = require('http');
+
+
+
+http.createServer(function (req, res) {
+  handler(req, res, function (err) {
+    res.statusCode = 404
+    res.end('no such location')
+  });
+}).listen(7777)
+
+
+
